b390532de72b9b3b3027d62a3be1b6cb0cf6e53d
Sofia Papagiannaki <sofia@grafana.com>
2019-09-27 17:16:50 +0300

    Add more tests

--- pkg/mathexp/resample_test.go
+++ pkg/mathexp/resample_test.go
@@ -20,7 +20,7 @@ func TestResampleSeries(t *testing.T) {
 		series           Series
 	}{
 		{
-			name:     "resample series; time range shorter than the rule interval",
+			name:     "resample series: time range shorter than the rule interval",
 			interval: "5S",
 			timeRange: &datasource.TimeRange{
 				FromRaw: fmt.Sprintf("%v", time.Unix(0, 0).Unix()+1e3),
@@ -34,8 +34,50 @@ func TestResampleSeries(t *testing.T) {
 			errIs: assert.Error,
 		},
 		{
-			name:     "resample series",
+			name:     "resample series: invalid time range",
 			interval: "5S",
+			timeRange: &datasource.TimeRange{
+				FromRaw: fmt.Sprintf("%v", time.Unix(11, 0).Unix()+1e3),
+				ToRaw:   fmt.Sprintf("%v", time.Unix(0, 0).Unix()+1e3),
+			},
+			seriesToResample: makeSeries("", nil, tp{
+				unixTimePointer(2, 0), float64Pointer(2),
+			}, tp{
+				unixTimePointer(7, 0), float64Pointer(1),
+			}),
+			errIs: assert.Error,
+		},
+		{
+			name:     "resample series: downsampling (mean aggregation)",
+			interval: "5S",
+			timeRange: &datasource.TimeRange{
+				FromRaw: fmt.Sprintf("%v", time.Unix(0, 0).Unix()*1e3),
+				ToRaw:   fmt.Sprintf("%v", time.Unix(16, 0).Unix()*1e3),
+			},
+			seriesToResample: makeSeries("", nil, tp{
+				unixTimePointer(2, 0), float64Pointer(2),
+			}, tp{
+				unixTimePointer(4, 0), float64Pointer(3),
+			}, tp{
+				unixTimePointer(7, 0), float64Pointer(1),
+			}, tp{
+				unixTimePointer(9, 0), float64Pointer(2),
+			}),
+			errIs:    assert.NoError,
+			seriesIs: assert.Equal,
+			series: makeSeries("", nil, tp{
+				unixTimePointer(0, 0), nil,
+			}, tp{
+				unixTimePointer(5, 0), float64Pointer(2.5),
+			}, tp{
+				unixTimePointer(10, 0), float64Pointer(1.5),
+			}, tp{
+				unixTimePointer(15, 0), float64Pointer(2),
+			}),
+		},
+		{
+			name:     "resample series: upsampling (bfill)",
+			interval: "2S",
 			timeRange: &datasource.TimeRange{
 				FromRaw: fmt.Sprintf("%v", time.Unix(0, 0).Unix()*1e3),
 				ToRaw:   fmt.Sprintf("%v", time.Unix(11, 0).Unix()*1e3),
@@ -48,9 +90,17 @@ func TestResampleSeries(t *testing.T) {
 			errIs:    assert.NoError,
 			seriesIs: assert.Equal,
 			series: makeSeries("", nil, tp{
-				unixTimePointer(0, 0), float64Pointer(2),
+				unixTimePointer(0, 0), nil,
 			}, tp{
-				unixTimePointer(5, 0), float64Pointer(1),
+				unixTimePointer(2, 0), float64Pointer(2),
+			}, tp{
+				unixTimePointer(4, 0), float64Pointer(2),
+			}, tp{
+				unixTimePointer(6, 0), float64Pointer(2),
+			}, tp{
+				unixTimePointer(8, 0), float64Pointer(1),
+			}, tp{
+				unixTimePointer(10, 0), float64Pointer(1),
 			}),
 		},
 	}
