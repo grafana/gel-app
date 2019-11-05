ba8b1fc5de8003924705a25ab5b7d8f4c3a05d2c
Kyle Brandt <kyle@grafana.com>
2019-11-05 14:20:17 -0500

    testing: add service tests for gel (#46)
    replacing commented out tests for old service incarnation.

--- pkg/gelpoc/service_test.go
+++ pkg/gelpoc/service_test.go
@@ -1,323 +1,88 @@
 package gelpoc
 
-var (
-	pluginID = "test-plugin"
-	dsRefID  = "GA"
+import (
+	"context"
+	"encoding/json"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/grafana/grafana-plugin-sdk-go/dataframe"
+	"github.com/grafana/grafana-plugin-sdk-go/datasource"
+	"github.com/grafana/grafana-plugin-sdk-go/transform"
+	"github.com/stretchr/testify/require"
 )
 
-// func TestService_Scenario(t *testing.T) {
-// 	tsdb.RegisterTsdbQueryEndpoint(pluginID, func(ds *models.DataSource) (tsdb.TsdbQueryEndpoint, error) {
-// 		return newTestQueryEndpoint(
-// 			dsRefID,
-// 			pt(1567345500283, 0.0),
-// 			pt(1567346100283, 10.0),
-// 		), nil
-// 	})
+func TestService(t *testing.T) {
 
-// 	var (
-// 		rr = routing.NewRouteRegister()
-// 		cs = newTestCacheService(pluginID)
-// 	)
+	dsDF := dataframe.New("test", nil,
+		dataframe.NewField("time", dataframe.FieldTypeTime, []*time.Time{utp(1)}),
+		dataframe.NewField("value", dataframe.FieldTypeNumber, []*float64{fp(2)}))
 
-// 	svc := &GelService{
-// 		RouterRegister:  rr,
-// 		DatasourceCache: cs,
-// 	}
+	m := newMockGrafanaAPI(dsDF)
 
-// 	if err := svc.Init(); err != nil {
-// 		t.Fatal(err)
-// 	}
+	s := Service{m}
 
-// 	ga := simplejson.New()
-// 	ga.Set("datasource", "gdev-testdata")
-// 	ga.Set("refId", dsRefID)
-// 	ga.Set("datasourceId", 123)
+	tr := datasource.TimeRange{
+		From: time.Unix(0, 0),
+		To:   time.Unix(1, 0),
+	}
+	queries := []transform.Query{
+		transform.Query{
+			RefID:     "A",
+			ModelJSON: json.RawMessage(`{ "datasource": "test", "datasourceId": 3, "orgId": 1, "intervalMs": 1000, "maxDataPoints": 1000 }`),
+		},
+		transform.Query{
+			RefID:     "B",
+			ModelJSON: json.RawMessage(`{ "datasource": "__expr__", "datasourceId": -100, "type": "math", "expression": "$A * 2" }`),
+		},
+	}
 
-// 	gb := simplejson.New()
-// 	gb.Set("datasource", gelDataSourceName)
-// 	gb.Set("refId", "GB")
-// 	gb.Set("type", "reduce")
-// 	gb.Set("reducer", "mean")
-// 	gb.Set("expression", "$GA")
+	pl, err := s.BuildPipeline(tr, queries)
+	require.NoError(t, err)
 
-// 	gc := simplejson.New()
-// 	gc.Set("datasource", gelDataSourceName)
-// 	gc.Set("refId", "GC")
-// 	gc.Set("type", "math")
-// 	gc.Set("expression", "$GA + $GB")
+	res, err := s.ExecutePipeline(context.Background(), pl)
+	require.NoError(t, err)
 
-// 	gd := simplejson.New()
-// 	gd.Set("datasource", gelDataSourceName)
-// 	gd.Set("refId", "GD")
-// 	gd.Set("type", "reduce")
-// 	gd.Set("reducer", "sum")
-// 	gd.Set("expression", "$GC")
+	bDF := dataframe.New("", nil,
+		dataframe.NewField("Time", dataframe.FieldTypeTime, []*time.Time{utp(1)}),
+		dataframe.NewField("", dataframe.FieldTypeNumber, []*float64{fp(4)}))
+	bDF.RefID = "B"
 
-// 	tr := tsdb.NewTimeRange("", "")
+	expect := []*dataframe.Frame{dsDF, bDF}
 
-// 	p, err := buildPipeline([]*simplejson.Json{ga, gb, gc, gd}, tr, cs)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+	if diff := cmp.Diff(expect, res); diff != "" {
+		t.Errorf("Result mismatch (-want +got):\n%s", diff)
+	}
 
-// 	ctx := models.ReqContext{}
+}
 
-// 	mctx := macaron.Context{}
-// 	mctx.Req = macaron.Request{httptest.NewRequest("GET", "/", nil)}
+type mockGrafanaAPI struct {
+	QueryDatasourceFn func() ([]datasource.DatasourceQueryResult, error)
+}
 
-// 	ctx.Context = &mctx
+func newMockGrafanaAPI(df ...*dataframe.Frame) *mockGrafanaAPI {
+	return &mockGrafanaAPI{
+		QueryDatasourceFn: func() ([]datasource.DatasourceQueryResult, error) {
+			return []datasource.DatasourceQueryResult{
+				datasource.DatasourceQueryResult{
+					DataFrames: df,
+				},
+			}, nil
 
-// 	vars, err := p.Execute(&ctx)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
+		},
+	}
+}
 
-// 	if len(vars) != 4 {
-// 		t.Errorf("unexpected number of variables: %v", len(vars))
-// 	}
+func (m *mockGrafanaAPI) QueryDatasource(ctx context.Context, orgID int64, datasourceID int64, tr datasource.TimeRange, queries []datasource.Query) ([]datasource.DatasourceQueryResult, error) {
+	return m.QueryDatasourceFn()
+}
 
-// 	for _, val := range vars["GA"].Values {
-// 		df := val.AsDataFrame()
-// 		ts := fieldsAsTime(df.Fields)
-// 		fs := fieldsAsFloat64(df.Fields)
+func utp(sec int64) *time.Time {
+	t := time.Unix(sec, 0)
+	return &t
+}
 
-// 		if len(ts) != len(fs) {
-// 			t.Error("vectors should be of same length")
-// 		}
-// 		if want := 0.0; *fs[0] != want {
-// 			t.Errorf("got = %v; want = %v", *fs[0], want)
-// 		}
-// 		if want := 10.0; *fs[1] != want {
-// 			t.Errorf("got = %v; want = %v", *fs[1], want)
-// 		}
-// 	}
-
-// 	for _, val := range vars["GB"].Values {
-// 		df := val.AsDataFrame()
-// 		fs := fieldsAsFloat64(df.Fields)
-
-// 		if len(fs) != 1 {
-// 			t.Error("mean should only be one number")
-// 		}
-// 		if want := 5.0; *fs[0] != want {
-// 			t.Errorf("got = %v; want = %v", *fs[0], want)
-// 		}
-// 	}
-
-// 	for _, val := range vars["GC"].Values {
-// 		df := val.AsDataFrame()
-// 		ts := fieldsAsTime(df.Fields)
-// 		fs := fieldsAsFloat64(df.Fields)
-
-// 		if len(ts) != len(fs) {
-// 			t.Error("vectors should be of same length")
-// 		}
-// 		if want := 5.0; *fs[0] != want {
-// 			t.Errorf("got = %v; want = %v", *fs[0], want)
-// 		}
-// 		if want := 15.0; *fs[1] != want {
-// 			t.Errorf("got = %v; want = %v", *fs[1], want)
-// 		}
-// 	}
-
-// 	for _, val := range vars["GD"].Values {
-// 		df := val.AsDataFrame()
-// 		fs := fieldsAsFloat64(df.Fields)
-
-// 		if len(fs) != 1 {
-// 			t.Error("sum should only be one number")
-// 		}
-// 		if want := 20.0; *fs[0] != want {
-// 			t.Errorf("got = %v; want = %v", *fs[0], want)
-// 		}
-// 	}
-// }
-
-// Helpers
-
-// func pt(ts, val float64) tsdb.TimePoint {
-// 	return tsdb.TimePoint{null.FloatFrom(val), null.FloatFrom(ts)}
-// }
-
-// func fieldsAsTime(fs data.Fields) []*time.Time {
-// 	var res []*time.Time
-// 	for _, field := range fs {
-// 		if v, ok := field.Vector.(*data.TimeVector); ok {
-// 			for _, value := range *v {
-// 				res = append(res, value)
-// 			}
-// 		}
-// 	}
-// 	return res
-// }
-
-// func fieldsAsFloat64(fs data.Fields) []*float64 {
-// 	var res []*float64
-// 	for _, field := range fs {
-// 		if v, ok := field.Vector.(*data.Float64Vector); ok {
-// 			for _, value := range *v {
-// 				res = append(res, value)
-// 			}
-// 		}
-// 	}
-// 	return res
-// }
-
-// // Mocks
-
-// type testCacheService struct {
-// 	GetDatasourceFn func(datasourceID int64, user *models.SignedInUser, skipCache bool) (*models.DataSource, error)
-// }
-
-// func newTestCacheService(datasourceType string) *testCacheService {
-// 	return &testCacheService{
-// 		GetDatasourceFn: func(datasourceID int64, user *models.SignedInUser, skipCache bool) (*models.DataSource, error) {
-// 			return &models.DataSource{
-// 				Type: pluginID,
-// 			}, nil
-// 		},
-// 	}
-// }
-
-// func (s *testCacheService) GetDatasource(datasourceID int64, user *models.SignedInUser, skipCache bool) (*models.DataSource, error) {
-// 	return s.GetDatasourceFn(datasourceID, user, skipCache)
-// }
-
-// type testQueryEndpoint struct {
-// 	QueryFn func(ctx context.Context, ds *models.DataSource, query *tsdb.TsdbQuery) (*tsdb.Response, error)
-// }
-
-// func newTestQueryEndpoint(refID string, points ...tsdb.TimePoint) *testQueryEndpoint {
-// 	return &testQueryEndpoint{
-// 		QueryFn: func(ctx context.Context, ds *models.DataSource, query *tsdb.TsdbQuery) (*tsdb.Response, error) {
-// 			return &tsdb.Response{
-// 				Results: map[string]*tsdb.QueryResult{
-// 					refID: &tsdb.QueryResult{
-// 						RefId: refID,
-// 						Series: tsdb.TimeSeriesSlice{
-// 							&tsdb.TimeSeries{
-// 								Name:   refID + "-series",
-// 								Points: tsdb.TimeSeriesPoints(points),
-// 							},
-// 						},
-// 					},
-// 				},
-// 			}, nil
-// 		},
-// 	}
-// }
-
-// func (e *testQueryEndpoint) Query(ctx context.Context, ds *models.DataSource, query *tsdb.TsdbQuery) (*tsdb.Response, error) {
-// 	return e.QueryFn(ctx, ds, query)
-// }
-
-// func TestReqGoNumPlay(t *testing.T) {
-// 	s := &Service{}
-// 	aGelBlob := []byte(`
-// 	{
-// 		"datasource": "-- GEL --",
-// 		"type": "math",
-// 		"refId": "GA",
-// 		"expression": "1 + $GB"
-// 	}
-// 	`)
-// 	bGelBlob := []byte(`
-// 	{
-// 		"datasource": "-- GEL --",
-// 		"type": "math",
-// 		"refId": "GB",
-// 		"expression": "1 + $GC"
-// 	}
-// 	`)
-// 	datasourceBlob := []byte(`
-// 	{
-// 		"refId": "GC",
-// 		"datasource": "gdev-testdata",
-// 		"scenarioId": "predictable_pulse",
-// 		"stringInput": "",
-// 		"pulseWave": {
-// 			"timeStep": 60,
-// 			"onCount": 3,
-// 			"onValue": 2,
-// 			"offCount": 3,
-// 			"offValue": 1
-// 		},
-// 		"datasourceId": 4
-// 	}
-// 	`)
-// 	dGelBlob := []byte(`
-// 	{
-// 		"datasource": "-- GEL --",
-// 		"type": "math",
-// 		"refId": "GD",
-// 		"expression": "1 + $GE"
-// 	}
-// 	`)
-// 	eGelBlob := []byte(`
-// 	{
-// 		"datasource": "-- GEL --",
-// 		"type": "math",
-// 		"refId": "GE",
-// 		"expression": "1"
-// 	}
-// 	`)
-
-// 	aGelSJSON, err := simplejson.NewJson(aGelBlob)
-// 	if err != nil {
-// 		t.FailNow()
-// 	}
-// 	bGelSJSON, err := simplejson.NewJson(bGelBlob)
-// 	if err != nil {
-// 		t.FailNow()
-// 	}
-// 	dGelSJSON, err := simplejson.NewJson(dGelBlob)
-// 	if err != nil {
-// 		t.FailNow()
-// 	}
-// 	eGelSJSON, err := simplejson.NewJson(eGelBlob)
-// 	if err != nil {
-// 		t.FailNow()
-// 	}
-// 	dsJSON, err := simplejson.NewJson(datasourceBlob)
-// 	if err != nil {
-// 		t.FailNow()
-// 	}
-// 	req := GelAppReq{
-// 		ReqOptions{
-// 			Targets: []*simplejson.Json{
-// 				aGelSJSON,
-// 				bGelSJSON,
-// 				dsJSON,
-// 				dGelSJSON, // d and e are connect to each other, but not the rest of the graph
-// 				eGelSJSON, //
-// 				// P.O.S !@#$ simplejson seems to ignore struct tags
-// 				// simplejson.NewFromAny(
-// 				// 	&struct {
-// 				// 		Datasource string `json:"datasource"`
-// 				// 		Type       string `json:"type"`
-// 				// 		RefID      string `json:"refId"`
-// 				// 		Expr       string `json:"expression"`
-// 				// 	}{
-// 				// 		gelDataSourceName,
-// 				// 		"math",
-// 				// 		"$GB",
-// 				// 		"1 + $GA",
-// 				// 	},
-// 				// ),
-// 			},
-// 		},
-// 	}
-
-// 	_ = req
-// 	_ = s
-// 	// TODO Fix me
-
-// 	// nodes, err := buildPipeline(
-// 	// 	req.Options.Targets,
-// 	// 	tsdb.NewTimeRange(req.Options.Range.Raw.From, req.Options.Range.Raw.To),
-// 	// 	s.DatasourceCache,
-// 	// )
-
-// 	// spew.Dump(err)
-// 	// spew.Dump(nodes)
-// }
+func fp(f float64) *float64 {
+	return &f
+}
