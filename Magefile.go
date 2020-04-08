e1950d1a2e8dd029b272177351d7ebcc74ecc294
ryantxu <ryantxu@gmail.com>
2020-04-07 23:51:33 -0700

    make a zip file

--- Magefile.go
+++ Magefile.go
@@ -3,6 +3,10 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"path"
 
 	// mage:import
 	_ "github.com/grafana/grafana-plugin-sdk-go/build"
@@ -27,6 +31,33 @@ func CopyArtifacts() error {
 	if err := sh.RunV("cp", "./src/pipeline.svg", "./dist"); err != nil {
 		return err
 	}
+	return nil
+}
+
+func readPluginJSON() (map[string]interface{}, error) {
+	byteValue, err := ioutil.ReadFile(path.Join("src", "plugin.json"))
+	if err != nil {
+		return nil, err
+	}
+
+	var result map[string]interface{}
+	err = json.Unmarshal([]byte(byteValue), &result)
+	return result, err
+}
+
+func MakePluginZip() error {
+	pluginJson, err := readPluginJSON()
+	if err != nil {
+		return err
+	}
+
+	zip := fmt.Sprintf("%s-%s.zip", pluginJson["id"], pluginJson["version"])
+
+	// TODO: pick final name based on the contents of plugin.json
+	if err := sh.RunV("zip", "-r", zip, "dist"); err != nil {
+		return err
+	}
+
 	return nil
 }
 
