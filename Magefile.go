45c29ee349fb411a309f908fbccdcac2fff2d227
Arve Knudsen <arve.knudsen@gmail.com>
2020-04-09 18:28:00 +0200

    Merge with Ryan's Mage PR
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- Magefile.go
+++ Magefile.go
@@ -3,9 +3,17 @@
 package main
 
 import (
+	"archive/zip"
+	"encoding/json"
+	"fmt"
+	"github.com/magefile/mage/mg"
+	"io"
 	"io/ioutil"
+	"log"
 	"os"
 	"path"
+	"path/filepath"
+	"strings"
 
 	// mage:import
 	"github.com/grafana/grafana-plugin-sdk-go/build"
@@ -13,6 +21,8 @@ import (
 )
 
 func CopyArtifacts() error {
+	// TODO: Only try to remove if dist exists, check error
+	_ = os.RemoveAll("dist")
 	if err := os.MkdirAll("dist", 0775); err != nil {
 		return err
 	}
@@ -35,5 +45,83 @@ func CopyArtifacts() error {
 	return nil
 }
 
+func readPluginJSON() (map[string]interface{}, error) {
+	byteValue, err := ioutil.ReadFile(path.Join("src", "plugin.json"))
+	if err != nil {
+		return nil, err
+	}
+
+	var result map[string]interface{}
+	err = json.Unmarshal([]byte(byteValue), &result)
+	return result, err
+}
+
+// Zip builds the plugin zip archive.
+func Zip() error {
+	mg.Deps(CopyArtifacts, build.BuildAll)
+
+	pluginJson, err := readPluginJSON()
+	if err != nil {
+		return err
+	}
+
+	pluginName := fmt.Sprintf("%s-%s", pluginJson["id"], pluginJson["version"])
+	zipFname := fmt.Sprintf("%s.zip", pluginName)
+	log.Printf("Creating zip archive %q", zipFname)
+	f, err := os.OpenFile(zipFname, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	zw := zip.NewWriter(f)
+	defer zw.Close()
+
+	if err := filepath.Walk("dist", func(srcPath string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Printf("filepath.Walk failed for %q: %s", srcPath, err)
+			return err
+		}
+
+		fi, err := os.Lstat(srcPath)
+		if err != nil {
+			return err
+		}
+		fh, err := zip.FileInfoHeader(fi)
+		if err != nil {
+			return err
+		}
+		fh.Name = path.Join(pluginName, strings.TrimPrefix(srcPath, "dist"))
+		log.Printf("Adding %q to zip archive as %q", srcPath, fh.Name)
+
+		if info.IsDir() {
+			fh.Name += "/"
+		}
+		fw, err := zw.CreateHeader(fh)
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			log.Printf("Adding directory to zip archive: %q", fh.Name)
+			return nil
+		}
+
+		src, err := os.Open(srcPath)
+		if err != nil {
+			return err
+		}
+		defer src.Close()
+		_, err = io.Copy(fw, src)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // Default configures the default target.
-var Default = build.BuildAll
+var Default = Zip
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/grafana/grafana-plugin-sdk-go v0.39.1-0.20200409123722-80ebc70a6ef5
 	github.com/hashicorp/go-hclog v0.8.0 // indirect
 	github.com/hashicorp/go-plugin v1.2.2
+	github.com/magefile/mage v1.9.0
 	github.com/stretchr/testify v1.4.0
 	golang.org/x/net v0.0.0-20190923162816-aa69164e4478
 	gonum.org/v1/gonum v0.0.0-20190923210107-40fa6a493b3d
