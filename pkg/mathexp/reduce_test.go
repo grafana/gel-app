2e64f31aeffcb6a7a3714395ea8e949fe87149cf
Kyle Brandt <kyle@grafana.com>
2020-08-03 11:42:30 -0400

    reduce: do not panic on nil values (#77)

--- pkg/mathexp/reduce_test.go
+++ pkg/mathexp/reduce_test.go
@@ -1,11 +1,34 @@
 package mathexp
 
 import (
+	"math"
 	"testing"
+	"time"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/grafana/grafana-plugin-sdk-go/data"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+var seriesWithNil = Vars{
+	"A": Results{
+		[]Value{
+			makeSeries("temp", nil, tp{
+				time.Unix(5, 0), float64Pointer(2),
+			}, tp{
+				time.Unix(10, 0), nil,
+			}),
+		},
+	},
+}
+
+var seriesEmpty = Vars{
+	"A": Results{
+		[]Value{
+			makeSeries("temp", nil),
+		},
+	},
+}
 
 func TestSeriesReduce(t *testing.T) {
 	var tests = []struct {
@@ -13,20 +36,132 @@ func TestSeriesReduce(t *testing.T) {
 		red         string
 		vars        Vars
 		varToReduce string
-		errIs       assert.ErrorAssertionFunc
-		resultsIs   assert.ComparisonAssertionFunc
+		errIs       require.ErrorAssertionFunc
+		resultsIs   require.ComparisonAssertionFunc
 		results     Results
 	}{
+		{
+			name:        "foo reduction will error",
+			red:         "foo",
+			varToReduce: "A",
+			vars:        aSeriesNullableTime,
+			errIs:       require.Error,
+			resultsIs:   require.Equal,
+		},
 		{
 			name:        "sum series",
 			red:         "sum",
 			varToReduce: "A",
 			vars:        aSeriesNullableTime,
-			errIs:       assert.NoError,
-			resultsIs:   assert.Equal,
+			errIs:       require.NoError,
+			resultsIs:   require.Equal,
 			results: Results{
 				[]Value{
 					makeNumber("sum_", nil, float64Pointer(3)),
+				},
+			},
+		},
+		{
+			name:        "sum series with a nil value",
+			red:         "sum",
+			varToReduce: "A",
+			vars:        seriesWithNil,
+			errIs:       require.NoError,
+			resultsIs:   require.Equal,
+			results: Results{
+				[]Value{
+					makeNumber("sum_", nil, NaN),
+				},
+			},
+		},
+		{
+			name:        "sum empty series",
+			red:         "sum",
+			varToReduce: "A",
+			vars:        seriesEmpty,
+			errIs:       require.NoError,
+			resultsIs:   require.Equal,
+			results: Results{
+				[]Value{
+					makeNumber("sum_", nil, float64Pointer(0)),
+				},
+			},
+		},
+		{
+			name:        "mean series with a nil value",
+			red:         "mean",
+			varToReduce: "A",
+			vars:        seriesWithNil,
+			errIs:       require.NoError,
+			resultsIs:   require.Equal,
+			results: Results{
+				[]Value{
+					makeNumber("mean_", nil, NaN),
+				},
+			},
+		},
+		{
+			name:        "mean empty series",
+			red:         "mean",
+			varToReduce: "A",
+			vars:        seriesEmpty,
+			errIs:       require.NoError,
+			resultsIs:   require.Equal,
+			results: Results{
+				[]Value{
+					makeNumber("mean_", nil, NaN),
+				},
+			},
+		},
+		{
+			name:        "min series with a nil value",
+			red:         "min",
+			varToReduce: "A",
+			vars:        seriesWithNil,
+			errIs:       require.NoError,
+			resultsIs:   require.Equal,
+			results: Results{
+				[]Value{
+					makeNumber("min_", nil, NaN),
+				},
+			},
+		},
+		{
+			name:        "min empty series",
+			red:         "min",
+			varToReduce: "A",
+			vars:        seriesEmpty,
+			errIs:       require.NoError,
+			resultsIs:   require.Equal,
+			results: Results{
+				[]Value{
+					makeNumber("min_", nil, NaN),
+				},
+			},
+		},
+		{
+			name:        "max series with a nil value",
+			red:         "max",
+			varToReduce: "A",
+			vars:        seriesWithNil,
+			errIs:       require.NoError,
+			resultsIs:   require.Equal,
+			results: Results{
+				[]Value{
+					makeNumber("max_", nil, NaN),
+				},
+			},
+		},
+		{
+			name:        "max empty series",
+			red:         "max",
+			varToReduce: "A",
+			vars:        seriesEmpty,
+			errIs:       require.NoError,
+			resultsIs:   require.Equal,
+			results: Results{
+				[]Value{
+					makeNumber("max_", nil, NaN),
 				},
 			},
 		},
@@ -35,11 +170,24 @@ func TestSeriesReduce(t *testing.T) {
 			red:         "mean",
 			varToReduce: "A",
 			vars:        aSeriesNullableTime,
-			errIs:       assert.NoError,
-			resultsIs:   assert.Equal,
+			errIs:       require.NoError,
+			resultsIs:   require.Equal,
 			results: Results{
 				[]Value{
 					makeNumber("mean_", nil, float64Pointer(1.5)),
+				},
+			},
+		},
+		{
+			name:        "count empty series",
+			red:         "count",
+			varToReduce: "A",
+			vars:        seriesEmpty,
+			errIs:       require.NoError,
+			resultsIs:   require.Equal,
+			results: Results{
+				[]Value{
+					makeNumber("count_", nil, float64Pointer(0)),
 				},
 			},
 		},
@@ -58,8 +206,8 @@ func TestSeriesReduce(t *testing.T) {
 					},
 				},
 			},
-			errIs:     assert.NoError,
-			resultsIs: assert.Equal,
+			errIs:     require.NoError,
+			resultsIs: require.Equal,
 			results: Results{
 				[]Value{
 					makeNumber("mean_", data.Labels{"host": "a"}, float64Pointer(1.5)),
@@ -67,7 +215,6 @@ func TestSeriesReduce(t *testing.T) {
 			},
 		},
 	}
-	//Vars{
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -77,11 +224,17 @@ func TestSeriesReduce(t *testing.T) {
 				ns, err := series.Value().(*Series).Reduce(tt.red)
 				tt.errIs(t, err)
 				if err != nil {
-					t.Fail()
+					return
 				}
 				results.Values = append(results.Values, ns)
 			}
-			tt.resultsIs(t, tt.results, results)
+			opt := cmp.Comparer(func(x, y float64) bool {
+				return (math.IsNaN(x) && math.IsNaN(y)) || x == y
+			})
+			options := append([]cmp.Option{opt}, data.FrameTestCompareOptions()...)
+			if diff := cmp.Diff(tt.results, results, options...); diff != "" {
+				t.Errorf("Result mismatch (-want +got):\n%s", diff)
+			}
 		})
 	}
 }
